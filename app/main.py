e112214881bd39b52168d10111fd69255424cb0a
ERIKAYOSSA <erikayossa0507@gmail.com>
2025-09-08 16:28:36 +0100

    on reinitialisa tout

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# 📁 Montre le dossier static (CSS, JS, images)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# 📁 Dossier des templates HTML
+templates = Jinja2Templates(directory="app/templates")
+
+# 🏠 Route pour la page d’accueil
 @app.get("/", response_class=HTMLResponse)
-def index():
-    with open("index.html", "r", encoding="utf-8") as f:
-        return f.read()
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
