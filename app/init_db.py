35026c1ccbb88a8a20d442ef5f2d84670defa340
ERIKAYOSSA <erikayossa0507@gmail.com>
2025-09-11 18:53:04 +0100

    tout ceci avance

--- app/init_db.py
+++ app/init_db.py
@@ -1,8 +1,11 @@
 import asyncio
 from app.database import engine, Base
+from app.models.user import User  # ← indispensable
 
 async def init_models():
     async with engine.begin() as conn:
+        print("🔧 Creating tables...")
         await conn.run_sync(Base.metadata.create_all)
+        print("✅ Tables created successfully.")
 
 asyncio.run(init_models())
